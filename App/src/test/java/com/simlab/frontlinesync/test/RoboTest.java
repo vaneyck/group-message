6c9a15d0f24cbec9ff95f48c8b966c29989ea9b1
Vaneyck <vaneycksemeotoki@gmail.com>
2014-03-21 07:39:56 +0300

    Added roboelextric tests

---
+++ App/src/test/java/com/simlab/frontlinesync/test/RoboTest.java
@@ -0,0 +1,15 @@
+package com.simlab.frontlinesync.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class RoboTest {
+	@Test
+	public void testTrueIsTrue() throws Exception {
+		assertEquals(true, false);
+	}
+}
