740beb9219ccab983d80ae9c83cd3d7ff93fc467
Vaneyck <vaneycksemeotoki@gmail.com>
2015-11-30 12:27:22 +0300

    Can re-queue failed Dispatch

--- App/src/main/java/com/vanks/groupmessage/activities/MainActivity.java
+++ App/src/main/java/com/vanks/groupmessage/activities/MainActivity.java
@@ -48,8 +48,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
--- App/src/main/java/com/vanks/groupmessage/activities/ViewMessageActivity.java
+++ App/src/main/java/com/vanks/groupmessage/activities/ViewMessageActivity.java
@@ -1,13 +1,17 @@
 package com.vanks.groupmessage.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.vanks.groupmessage.R;
 import com.vanks.groupmessage.arrayadapters.view.DispatchArrayAdapter;
+import com.vanks.groupmessage.enums.DispatchStatus;
 import com.vanks.groupmessage.models.persisted.Dispatch;
 import com.vanks.groupmessage.models.persisted.Message;
 
@@ -37,6 +41,21 @@ public class ViewMessageActivity extends AppCompatActivity {
 		initialiseUi();
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu (Menu menu) {
+		getMenuInflater().inflate(R.menu.view, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected (MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_queue_failed) {
+			queueFailedDispatches();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 	private void initialiseUi () {
 		groupNameTextView = (TextView) findViewById(R.id.currentGroupNameTextView);
 		messageTextView = (TextView) findViewById(R.id.currentMessageTextView);
@@ -46,5 +65,14 @@ public class ViewMessageActivity extends AppCompatActivity {
 		dispatchArrayAdapter =  new DispatchArrayAdapter(this, R.layout.activity_dispatch_list_item, message.getDispatches());
 		dispatchListView.setAdapter(dispatchArrayAdapter);
 		dispatchArrayAdapter.notifyDataSetChanged();
+	}
+
+	private void queueFailedDispatches () {
+		for (Dispatch dispatch : message.getDispatches()) {
+			if(dispatch.getStatus() == DispatchStatus.FAILED) {
+				dispatch.setStatus(DispatchStatus.QUEUED);
+				dispatch.save();
+			}
+		}
 	}
 }
